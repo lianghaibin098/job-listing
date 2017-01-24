157b6d8b9b7e2988de6f811b796bdc4cd44f008c
lianghaibin <lianghaibin1688@gmail.com>
2017-01-24 22:55:07 +0800

    implement wage part

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -6,7 +6,7 @@ class Admin::JobsController < ApplicationController
     if !current_user.admin?
       flash[:alert] = "You are not admin"
       redirect_to root_path
-    end 
+    end
   end
 
   end
@@ -57,6 +57,6 @@ class Admin::JobsController < ApplicationController
   private
 
   def job_params
-    params.require(:job).permit(:title, :description)
+    params.require(:job).permit(:title, :description，:wage_upper_bound, :wage_lower_bound, :contact_email)
   end
 end
